90beb2f4a0911bcf7873358572910e92c5341165
Shubh Rastogi <54894915+shubhrastogi@users.noreply.github.com>
2024-12-24 04:37:07 +0530

    Time: 17 ms (15.06%), Space: 44.7 MB (39.02%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        // convert Strign s to lowerCase with regex
+        String tempString = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        // Reverse the tempString 
+        String rev = new StringBuffer(tempString).reverse().toString();
+        // check tempString to rev String 
+        return tempString.equals(rev);
+    }
+}
